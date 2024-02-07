eda123edc738fafa74da213603307dedefbf27a4
Faha Tagizade <Fakhriyya16>
2024-02-07 20:23:10 +0400

    homework assignments written in code

---
+++ Csharp-intro/Program.cs
@@ -0,0 +1,225 @@
+﻿//Tasks
+
+//task1
+
+//int n = 21;
+
+//if(n % 3 == 0 && n % 7 == 0)
+//{
+//    Console.WriteLine("Bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//}
+
+//task2
+
+//int n = 14;
+//int m = 6;
+//int sum = 0;
+
+//if(n % 2 == 0 && m % 2 == 0)
+//{
+//    sum = n + m;
+//}
+//Console.WriteLine(sum);
+
+//task3
+
+//int n = 6;
+//int m = 13;
+//int sum = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+//task4
+
+//int[] nums = { 3, 65, 13, 8, 45, 26, 5 };
+//int sum = 0;
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 1)
+//    {
+//        sum += nums[i];
+//    }
+//}
+//Console.WriteLine(sum);
+
+//task5
+
+//int[] nums = { 2, 5, 7, 34, 87, 12, 14 };
+//int count = 0;
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 0)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+//task6
+
+//int n = 11;
+//int m = 21;
+//int count = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+//task7
+
+//int n = 2;
+//int m = 11;
+//int sum = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if(i % 2 == 0)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+//task8
+
+//int n = 2;
+//int m = 10;
+//int count = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if(i % 2 == 0)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+//task9
+
+//int n = 132;
+//int count = 0;
+
+//for(int i = 1; i <= n; i++)
+//{
+//    if (n % i == 0)
+//    {
+//        count++;    
+//    }
+//}
+//if(count == 2)
+//{
+//    Console.WriteLine("sade");
+//}
+//else
+//{
+//    Console.WriteLine("murekkeb");
+//}
+
+//ClassRepetition
+
+//first
+
+//int n = 212;
+//int m = 14;
+//int sum = n + m;
+
+//Console.WriteLine(sum);
+
+//second
+
+//bool isMarried = false;
+
+//if (isMarried == true)
+//{
+//    Console.WriteLine("evlidir");
+//}
+//else 
+//{
+//    Console.WriteLine("subay");
+//}
+
+//third
+
+//int[] arr = { 1, 3, 4, 5, 6, 21 };
+
+//for(int i = 0; i < arr.Length; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//for(int i = 0;i < arr.Length; i++)
+//{
+//    Console.WriteLine(arr[i]);
+//}
+
+//forth
+
+//int a = 1;
+//int b = 100;
+
+//for (int i = a; i <= b; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//fifth
+
+//int a = 1;
+//int b = 100;
+
+//for(int i = a; i <= b; i++)
+//{
+//    if(i % 2 == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+//sixth
+
+//int a = 1;
+//int b = 100;
+//int result = 0;
+
+//for (int i = a; i <= b; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        result += i;
+//    }
+//}
+//Console.WriteLine(result);
+
+//seventh
+
+//string[] words = { "salam", "qreqw", "salam", "salam", "fejwqd" };
+
+//int count = 0;
+
+//for(int i = 0; i < words.Length; i++)
+//{
+//    if (words[i] == "salam")
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
